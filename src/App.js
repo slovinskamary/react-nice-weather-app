3548245fd5212d6fa07e7573bb95ae091bc70481
slovinskamary <slovinskamary@gmail.com>
2023-09-09 15:01:55 +0300

    fixed errors with target_blank

--- src/App.js
+++ src/App.js
@@ -6,13 +6,18 @@ export default function App() {
       <h1>Weather App</h1>
       <footer>
         This project is created by{" "}
-        <a href="https://www.instagram.com/masholik/" target="_blank">
+        <a
+          href="https://www.instagram.com/masholik/"
+          target="_blank"
+          rel="noreferrer"
+        >
           Mariia Slovinska
         </a>{" "}
         and is {""}
         <a
           href="https://github.com/slovinskamary/react-nice-weather-app"
           target="_blank"
+          rel="noreferrer"
         >
           open-sourced on GitHub
         </a>
