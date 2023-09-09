25347022f305190345bb7f3221b4e8af756e4966
slovinskamary <slovinskamary@gmail.com>
2023-09-09 16:44:00 +0300

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,25 +3,27 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project is created by{" "}
-        <a
-          href="https://www.instagram.com/masholik/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Mariia Slovinska
-        </a>{" "}
-        and is {""}
-        <a
-          href="https://github.com/slovinskamary/react-nice-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project is created by{" "}
+          <a
+            href="https://www.instagram.com/masholik/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Mariia Slovinska
+          </a>{" "}
+          and is {""}
+          <a
+            href="https://github.com/slovinskamary/react-nice-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
