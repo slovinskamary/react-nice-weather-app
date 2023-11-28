82b5bfdc459ce7762d28fa13746f5955dba240d2
slovinskamary <slovinskamary@gmail.com>
2023-11-28 21:51:44 +0200

    added describe icon (codeMapping)

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -1,19 +1,30 @@
 import React from "react";
 import ReactAnimatedWeather from "react-animated-weather";
 export default function WeatherIcon(props) {
-  if (props.data.code === "01d") {
+  const codeMapping = {
+    "clear-sky-day": "CLEAR_DAY",
+    "clear-sky-night": "CLEAR_NIGHT",
+    "few-clouds-day": "PARTLY_CLOUDY_DAY",
+    "few-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "scattered-clouds-day": "PARTLY_CLOUDY_DAY",
+    "scattered-clouds-night": "PARTLY_CLOUDY_NIGHT",
+    "broken-clouds-day": "CLOUDY",
+    "broken-clouds-night": "CLOUDY",
+    "shower-rain-day": "RAIN",
+    "shower-rain-night": "RAIN",
+    "rain-day": "RAIN",
+    "rain-night": "RAIN",
+    "thunderstorm-day": "RAIN",
+    "thunderstorm-night'": "RAIN",
+    "snow-day": "SNOW",
+    "snow-night": "SNOW",
+    "mist-day": "FOG",
+    "mist-night": "FOG",
+  };
+  {
     return (
       <ReactAnimatedWeather
-        icon="CLEAR DAY"
-        color="red"
-        size={64}
-        animate={true}
-      />
-    );
-  } else {
-    return (
-      <ReactAnimatedWeather
-        icon="CLEAR NIGHT"
+        icon={codeMapping[props.code]}
         color="red"
         size={64}
         animate={true}
