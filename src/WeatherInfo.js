6a0063b0688aaecc1805a4aedd4fdef021354057
slovinskamary <slovinskamary@gmail.com>
2023-12-03 22:20:24 +0200

    tried to fix bugs with icon

--- src/WeatherInfo.js
+++ src/WeatherInfo.js
@@ -21,7 +21,7 @@ export default function WeatherInfo(props) {
         <div className="col-lg-6 col-md-6 col-sm-8">
           <div className=" temperature-container d-flex justify-content-end">
             <div className="float-left">
-              <WeatherIcon code={props.data.icon} size={52} />
+               <img code={props.icon} /> 
             </div>
 
             <div>
